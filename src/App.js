0df292086d8d1c8fc9e806613d21c60b9c0c9494
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-03-17 12:32:30 -0300

    feat: Added basic UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App ">
+			<h1 className="display-4">Hi! My name is Bruno, nice to see you here.</h1>
+			<br />
+			<Button variant="outline-warning" size="lg" className=" btn mb-4 w-25">
+				Projects
+			</Button>
+			<Button variant="outline-warning" size="lg" className="btn w-25">
+				About
+			</Button>
+		</div>
+	);
 }
 
 export default App;
