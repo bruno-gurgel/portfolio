ecd6bfb895ca3beff745c4e1fdeeac13533c6943
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-03-18 12:06:05 -0300

    feat: Added Projects module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Switch } from "react-router";
 import About from "./features/about/About";
 import Home from "./features/home/Home";
+import Projects from "./features/projects/Projects";
 
 function App() {
 	return (
@@ -9,6 +10,9 @@ function App() {
 			<Switch>
 				<Route path="/about">
 					<About />
+				</Route>
+				<Route path="/projects">
+					<Projects />
 				</Route>
 				<Route path="/">
 					<Home />
---
+++ src/features/projects/Projects.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./projects.module.css";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import MyReadsBanner from "../../assets/myreads-banner.png";
+import WouldYouRatherBanner from "../../assets/would-you-rather-banner-1.png";
+import { Link } from "react-router-dom";
+
+export default function Projects() {
+	return (
+		<Container className="h-100">
+			<Link to="/">
+				<Button variant="outline-warning" size="lg" className="mt-1">
+					Go home
+				</Button>
+			</Link>
+			<div className={styles.cardBox}>
+				<Card className={styles.card}>
+					<Card.Header className="display-4">MyReads</Card.Header>
+					<Card.Body className={styles.cardBody}>
+						<Card.Title>
+							Bookshelf app made with React that allows you to select and categorize
+							books you have read, are currently reading, or want to read.
+						</Card.Title>
+						<div className={styles.btnContainer}>
+							<Button variant="dark">Details</Button>
+							<Button variant="dark">Repository</Button>
+						</div>
+					</Card.Body>
+				</Card>
+				<Card className={styles.card}>
+					<Card.Header className="display-4">Would you Rather...</Card.Header>
+					<Card.Body className={styles.cardBody}>
+						<Card.Title>
+							Variation of the Would You Rather game. made with Bootstrap, React &
+							Redux.
+						</Card.Title>
+						<div className={styles.btnContainer}>
+							<Button variant="dark">Details</Button>
+							<Button variant="dark">Repository</Button>
+						</div>
+					</Card.Body>
+				</Card>
+			</div>
+		</Container>
+	);
+}
