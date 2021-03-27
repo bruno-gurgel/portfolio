748d78258df516222d97ff0264cc80f099473d84
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-03-27 16:42:34 -0300

    feat: Added content to intro of the project page

--- src/features/projects/MyReads/index.js
+++ src/features/projects/MyReads/index.js
@@ -1,29 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MyReads.module.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
+import myReadsBanner from "./assets/myreads-banner.png";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import MyReadsPortuguese from "./portuguese/MyReads_portuguese";
 
 export default function MyReads() {
-	return (
-		<Container>
-			<Link to="/projects">
-				<Button variant="outline-warning" size="lg" className="mt-1">
-					<strong>Go back</strong>
-				</Button>
-			</Link>
-			<Container>
-				<h1 className="display-3 text-white mt-4">MyReads</h1>
-				<h2 className={styles.subHeading}>
-					Bookshelf app that allows you to select and categorize your books.
-				</h2>
-				<Link to={{ pathname: "https://github.com/bruno-gurgel/MyReads" }} target="_blank">
-					<Button variant="warning" size="lg">
-						<strong>Repository</strong>
+	const [language, updateLanguage] = useState("english");
+
+	if (language === "english") {
+		return (
+			<Container className={styles.myReadsContainer}>
+				<div className={styles.navBtnsBox}>
+					<Link to="/projects">
+						<Button variant="outline-warning" size="lg" className="mt-1">
+							<strong>Go back</strong>
+						</Button>
+					</Link>
+					<Button
+						variant="outline-warning"
+						size="lg"
+						className="mt-1"
+						onClick={() => updateLanguage("portuguese")}
+					>
+						<strong>Mudar para Português</strong>
 					</Button>
-				</Link>
+				</div>
+				<Container>
+					<h1 className="display-3 text-white mt-4">MyReads</h1>
+					<h2 className={styles.subHeading}>
+						Bookshelf app that allows you to select and categorize your books.
+					</h2>
+					<Button variant="warning" size="lg" className={styles.button}>
+						<Link
+							to={{ pathname: "https://github.com/bruno-gurgel/MyReads" }}
+							target="_blank"
+							className={styles.buttonLink}
+						>
+							<strong>Repository</strong>
+						</Link>
+					</Button>
+				</Container>
+				<Container className="mt-5">
+					<p className={styles.introText}>
+						In this project, I had to make a Single Page Application (SPA) that was a
+						bookshelf so that the users could organize what they were reading or wanting
+						to read.
+					</p>
+					<p className={styles.introText}>
+						I used an API server provided by Udacity and had to lay out the application
+						so that it was clean and reusable. I followed along with
+						<Link
+							to={{ pathname: "https://reactjs.org/docs/thinking-in-react.html" }}
+							target="_blank"
+							className={styles.pageLink}
+						>
+							{" "}
+							React documentation{" "}
+						</Link>{" "}
+						suggestions to make the project, but since this was my first project using
+						this UI library, there were some things that now I would do differently,
+						especially the mock/planning part, that if done correctly saves a lot of
+						time later.
+					</p>
+					<p className={styles.introText}>
+						I only used React in this application, always trying to employ best
+						practices like avoiding class components. Since I learned React without the
+						hooks, I faced some challenges, but I really learned a lot.
+					</p>
+					<div className={styles.buttonBox}>
+						<Button variant="warning" size="lg" className={styles.button}>
+							<Link to="/" className={styles.buttonLink}>
+								<strong>Here is the project so you can check it.</strong>
+							</Link>
+						</Button>
+					</div>
+				</Container>
 			</Container>
-		</Container>
-	);
+		);
+	} else {
+		return <MyReadsPortuguese onUpdateLanguage={updateLanguage} />;
+	}
 }
