d8e0531d300890ae7fba572a97fbeed1818594ff
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-03-27 16:42:57 -0300

    feat: Added option to change languages

---
+++ src/features/projects/MyReads/portuguese/MyReads_portuguese.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styles from "../MyReads.module.css";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+export default function MyReadsPortuguese({ onUpdateLanguage }) {
+	return (
+		<Container className={styles.myReadsContainer}>
+			<div className={styles.navBtnsBox}>
+				<Link to="/projects">
+					<Button variant="outline-warning" size="lg" className="mt-1">
+						<strong>Voltar</strong>
+					</Button>
+				</Link>
+				<Button
+					variant="outline-warning"
+					size="lg"
+					className="mt-1"
+					onClick={() => onUpdateLanguage("english")}
+				>
+					<strong>Change to English</strong>
+				</Button>
+			</div>
+			<Container>
+				<h1 className="display-3 text-white mt-4">MyReads</h1>
+				<h2 className={styles.subHeading}>
+					Aplicativo de estante virtual que permite selecionar e categorizar seus livros.
+				</h2>
+				<Button variant="warning" size="lg" className={styles.button}>
+					<Link
+						to={{ pathname: "https://github.com/bruno-gurgel/MyReads" }}
+						target="_blank"
+						className={styles.buttonLink}
+					>
+						<strong>Repositório</strong>
+					</Link>
+				</Button>
+			</Container>
+			<Container className="mt-5">
+				<p className={styles.introText}>
+					Neste projeto, tive que fazer um Single Page Application (SPA) que fosse uma
+					estante de livros para que os usuários pudessem organizar o que estavam lendo ou
+					querendo ler.
+				</p>
+				<p className={styles.introText}>
+					Usei uma API fornecida pela Udacity de servidor e tive que fazer o layout do
+					aplicativo para que ficasse limpo e reutilizável. Eu segui junto com as
+					sugestões da
+					<Link
+						to={{ pathname: "https://reactjs.org/docs/thinking-in-react.html" }}
+						target="_blank"
+						className={styles.pageLink}
+					>
+						{" "}
+						documentação do React{" "}
+					</Link>{" "}
+					para fazer o projeto, mas como este foi meu primeiro projeto neste framework,
+					algumas coisas eu faria de forma diferente hoje, especialmente a parte de
+					simulação/planejamento, que se feita corretamente economiza muito tempo depois.
+				</p>
+				<p className={styles.introText}>
+					Só usei o React neste aplicativo, sempre tentando empregar as melhores práticas,
+					como evitar componentes de classe. Desde que aprendi React sem os hooks,
+					enfrentei alguns desafios, mas realmente aprendi muito.
+				</p>
+				<div className={styles.buttonBox}>
+					<Button variant="warning" size="lg" className={styles.button}>
+						<Link to="/" className={styles.buttonLink}>
+							<strong>Aqui está o link para o projeto.</strong>
+						</Link>
+					</Button>
+				</div>
+			</Container>
+		</Container>
+	);
+}
