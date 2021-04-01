dcc81f9a2cbd22caaacaf6c2802669213b29bf9e
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-04-01 12:31:23 -0300

    style: Adjusted style

--- src/features/WouldYouRather/components/PollVote.js
+++ src/features/WouldYouRather/components/PollVote.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "../WouldYouRather.module.css";
 import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getAuthedUser } from "../redux/modules/authedUser";
@@ -7,79 +8,79 @@ import { fetchAllData } from "../redux/modules/shared";
 import PropTypes from "prop-types";
 
 export default function PollVote(props) {
-  const dispatch = useDispatch();
-  const authedUser = useSelector(getAuthedUser);
+	const dispatch = useDispatch();
+	const authedUser = useSelector(getAuthedUser);
 
-  const [selectedAnswer, updateSelectedAnswer] = useState(null);
+	const [selectedAnswer, updateSelectedAnswer] = useState(null);
 
-  const { authorName, authorAvatar, optionOne, optionTwo, questionID } = props;
+	const { authorName, authorAvatar, optionOne, optionTwo, questionID } = props;
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+	const onSubmit = (event) => {
+		event.preventDefault();
 
-    dispatch(
-      handleNewAnswer({ authedUser, qid: questionID, answer: selectedAnswer })
-    ).then(() => dispatch(fetchAllData()));
-  };
-  return (
-    <li className=" user-card w-50 mx-auto border">
-      <h5 className="card-header text-left ">{authorName} asks:</h5>
-      <div className="d-flex">
-        <div
-          className="contact-avatar avatar-preview"
-          style={{
-            backgroundImage: `url(/${authorAvatar})`,
-          }}
-        />
-        <div className="contact-details">
-          <h6 className="text-left pt-3">Would you rather:</h6>
-          <Form
-            onSubmit={onSubmit}
-            onChange={(event) => updateSelectedAnswer(event.target.value)}
-          >
-            <Form.Check
-              type="radio"
-              label={optionOne}
-              name="pollVote"
-              value="optionOne"
-              className="text-left"
-              required
-            />
-            <Form.Check
-              type="radio"
-              label={optionTwo}
-              name="pollVote"
-              value="optionTwo"
-              className="text-left"
-              required
-            />
-            <Button type="submit" className="w-100 button" variant="success">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </div>
-      <div className="p-2 border-top" id="icons-attribute">
-        Icons made by{" "}
-        <a
-          href="https://www.flaticon.com/authors/icongeek26"
-          title="Icongeek26"
-        >
-          Icongeek26
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
-    </li>
-  );
+		dispatch(
+			handleNewAnswer({ authedUser, qid: questionID, answer: selectedAnswer })
+		).then(() => dispatch(fetchAllData()));
+	};
+	return (
+		<div className={styles.pollVoteCard}>
+			<h5 className={styles.userCardTitle}>{authorName} asks:</h5>
+			<div className="d-flex">
+				<div
+					className={`${styles.userAvatar} ${styles.avatarPreview}`}
+					style={{
+						backgroundImage: `url(/${authorAvatar})`,
+					}}
+				/>
+				<div className={styles.userDetails}>
+					<h6 className={styles.heading6}>Would you rather:</h6>
+					<Form
+						className={styles.pollVoteForm}
+						onSubmit={onSubmit}
+						onChange={(event) => updateSelectedAnswer(event.target.value)}
+					>
+						<Form.Check
+							type="radio"
+							label={optionOne}
+							name="pollVote"
+							value="optionOne"
+							className={styles.pollVoteRadio}
+							// className="text-left"
+							required
+						/>
+						<Form.Check
+							type="radio"
+							label={optionTwo}
+							name="pollVote"
+							value="optionTwo"
+							className={styles.pollVoteRadio}
+							// className="text-left"
+							required
+						/>
+						<Button type="submit" className={styles.greenBtn}>
+							Submit
+						</Button>
+					</Form>
+				</div>
+			</div>
+			<div className="p-2 border-top" id="icons-attribute">
+				Icons made by{" "}
+				<a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26">
+					Icongeek26
+				</a>{" "}
+				from{" "}
+				<a href="https://www.flaticon.com/" title="Flaticon">
+					www.flaticon.com
+				</a>
+			</div>
+		</div>
+	);
 }
 
 PollVote.propTypes = {
-  authorName: PropTypes.string.isRequired,
-  authorAvatar: PropTypes.string.isRequired,
-  optionOne: PropTypes.string.isRequired,
-  optionTwo: PropTypes.string.isRequired,
-  questionID: PropTypes.string.isRequired,
+	authorName: PropTypes.string.isRequired,
+	authorAvatar: PropTypes.string.isRequired,
+	optionOne: PropTypes.string.isRequired,
+	optionTwo: PropTypes.string.isRequired,
+	questionID: PropTypes.string.isRequired,
 };
